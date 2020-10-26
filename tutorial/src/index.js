ff082629a73d53b5eeb5c61e024b4c8f2cb822df
Hartz-I <ahnafsadikinan@gmail.com>
2020-10-27 04:06:15 +0600

    started learning react js

---
+++ tutorial/src/index.js
@@ -0,0 +1,9 @@
+//we'll add a component. just react and function
+import React from "react"
+import ReactDom from "react-dom" //connect with root div in index.html
+
+function Greeting(){ //to let react know that this is special we capitalize the first letter
+  return <h4>This is Inan and this is my first component!</h4>; //it's jsx
+}
+
+ReactDom.render(<Greeting /> , document.getElementById('root')); //this is how we connect in react
